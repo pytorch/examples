23933fcb49b33cfc7e2737b6b94bd329396c7d58
Adam Paszke <adam.paszke@gmail.com>
2016-08-23 20:38:51 -0700

    Add MNIST example

---
+++ mnist/data.py
@@ -0,0 +1,81 @@
+import os
+import torch
+import json
+from tqdm import tqdm
+
+if not os.path.exists('data/raw/train-images-idx3-ubyte'):
+    try:
+        os.makedirs(os.path.join('data', 'raw'))
+        os.makedirs(os.path.join('data', 'processed'))
+    except FileExistsError:
+        pass
+    import urllib.request
+    import gzip
+    urls = [
+        'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
+        'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz',
+    ]
+    for url in urls:
+        print('Downloading ' + url)
+        data = urllib.request.urlopen(url)
+        filename = url.rpartition('/')[2]
+        file_path = os.path.join('data', 'raw', filename)
+        with open(file_path, 'wb') as f:
+            f.write(data.read())
+        with open(file_path.replace('.gz', ''), 'wb') as out_f, \
+             gzip.GzipFile(file_path) as zip_f:
+            out_f.write(zip_f.read())
+        os.unlink(file_path)
+
+def get_int(b):
+    return int.from_bytes(b, 'big')
+
+def read_label_file(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+        assert get_int(data[:4]) == 2049
+        length = get_int(data[4:8])
+        labels = [b for b in tqdm(data[8:], total=length)]
+        assert len(labels) == length
+        return torch.LongTensor(labels)
+
+def read_image_file(path):
+    with open(path, 'rb') as f:
+        data = f.read()
+        assert get_int(data[:4]) == 2051
+        length = get_int(data[4:8])
+        num_rows = get_int(data[8:12])
+        num_cols = get_int(data[12:16])
+        images = []
+        idx = 16
+        for l in tqdm(range(length)):
+            img = []
+            images.append(img)
+            for r in range(num_rows):
+                row = []
+                img.append(row)
+                for c in range(num_cols):
+                    row.append(data[idx])
+                    idx += 1
+        assert len(images) == length
+        return torch.FloatTensor(images)
+
+print("Loading training set")
+training_set = (
+    read_image_file('data/raw/train-images-idx3-ubyte'),
+    read_label_file('data/raw/train-labels-idx1-ubyte'),
+)
+print("Saving training set")
+with open('data/processed/training.pt', 'wb') as f:
+    torch.save(training_set, f)
+
+print("Loading test set")
+test_set = (
+    read_image_file('data/raw/t10k-images-idx3-ubyte'),
+    read_label_file('data/raw/t10k-labels-idx1-ubyte')
+)
+print("Saving test set")
+with open('data/processed/test.pt', 'wb') as f:
+    torch.save(test_set, f)
---
+++ mnist/main.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.cuda
+import torch.optim as optim
+from torch.autograd import Variable
+from tqdm import tqdm
+
+def print_header(msg):
+    print('===>', msg)
+
+# Data
+print_header('Loading data')
+with open('data/processed/training.pt', 'rb') as f:
+    training_set = torch.load(f)
+with open('data/processed/test.pt', 'rb') as f:
+    test_set = torch.load(f)
+
+training_data = training_set[0].view(-1, 1, 28, 28).div(255)
+training_labels = training_set[1]
+test_data = test_set[0].view(-1, 1, 28, 28).div(255)
+test_labels = test_set[1]
+
+del training_set
+del test_set
+
+# Model
+print_header('Building model')
+class Net(nn.Container):
+    def __init__(self):
+        super(LeNet, self).__init__(
+            conv1 = nn.Conv2d(1, 20, 5, 5),
+            pool1 = nn.MaxPooling2d(2, 2),
+            conv2 = nn.Conv2d(20, 50, 5, 5),
+            pool2 = nn.MaxPooling2d(2, 2),
+            fc1   = nn.Linear(800, 500),
+            fc2   = nn.Linear(500, 10),
+            relu  = nn.ReLU(),
+            softmax = nn.LogSoftmax(),
+        )
+
+    def __call__(self, x):
+        x = self.relu(self.pool1(self.conv1(x)))
+        x = self.relu(self.pool2(self.conv2(x)))
+        x = x.view(-1, 800)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        return self.softmax(x)
+
+model = Net().cuda()
+
+criterion = nn.ClassNLLCriterion()
+
+# Training settings
+BATCH_SIZE = 150
+TEST_BATCH_SIZE = 1000
+NUM_EPOCHS = 2
+
+optimizer = optim.SGD((model, criterion), lr=1e-2, momentum=0.9)
+
+def train(epoch):
+    batch_data = Variable(torch.cuda.FloatTensor(BATCH_SIZE, 1, 28, 28), requires_grad=False)
+    batch_targets = Variable(torch.cuda.FloatTensor(BATCH_SIZE), requires_grad=False)
+    for i in range(0, training_data.size(0), BATCH_SIZE):
+        batch_data.data[:] = training_data[i:i+BATCH_SIZE]
+        batch_targets.data[:] = training_labels[i:i+BATCH_SIZE]
+        loss = optimizer.step(batch_data, batch_targets)
+        model.zero_grad_parameters()
+
+        print('Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.4f}'.format(epoch,
+            i+BATCH_SIZE, training_data.size(0),
+            (i+BATCH_SIZE)/training_data.size(0)*100, loss))
+
+def test(epoch):
+    test_loss = 0
+    batch_data = Variable(torch.cuda.FloatTensor(TEST_BATCH_SIZE, 1, 28, 28), requires_grad=False)
+    batch_targets = Variable(torch.cuda.FloatTensor(TEST_BATCH_SIZE), requires_grad=False)
+    for i in range(0, test_data.size(0), TEST_BATCH_SIZE):
+        print('Testing model: {}/{}'.format(i, test_data.size(0)), end='\r')
+        batch_data.data[:] = test_data[i:i+TEST_BATCH_SIZE]
+        batch_targets.data[:] = test_labels[i:i+TEST_BATCH_SIZE]
+        test_loss += criterion(model(batch_data), batch_targets).data[0]
+
+    test_loss /= (test_data.size(0) / TEST_BATCH_SIZE) # criterion averages over batch size
+    print('TEST SET RESULTS:' + ' ' * 20)
+    print('Average loss: {:.4f}'.format(test_loss))
+
+for epoch in range(1, NUM_EPOCHS+1):
+    train(epoch)
+    test(epoch)
+
