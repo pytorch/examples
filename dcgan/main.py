99a4c64833f73f25a48f304de913fb7ac1bcbf32
soumith <soumith@fb.com>
2016-11-18 17:31:50 -0800

    fixing dcgan commandline

--- dcgan/main.py
+++ dcgan/main.py
@@ -16,17 +16,17 @@ from torch.autograd import Variable
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', required=True, help='cifar10 | lsun | imagenet | folder | lfw ')
 parser.add_argument('--dataroot', required=True, help='path to dataset')
-parser.add_argument('--workers', help='number of data loading workers', default=2)
-parser.add_argument('--batchSize', default=64, help='input batch size')
-parser.add_argument('--imageSize', default=64, help='the height / width of the input image to network')
-parser.add_argument('--nz', default=100, help='size of the latent z vector')
-parser.add_argument('--ngf', default=64)
-parser.add_argument('--ndf', default=64)
-parser.add_argument('--niter', default=25, help='number of epochs to train for')
-parser.add_argument('--lr', default=0.0002, help='learning rate, default=0.0002')
-parser.add_argument('--beta1', default=0.5, help='beta1 for adam. default=0.5')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
+parser.add_argument('--ngf', type=int, default=64)
+parser.add_argument('--ndf', type=int, default=64)
+parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
-parser.add_argument('--ngpu'  , default=1, help='number of GPUs to use')
+parser.add_argument('--ngpu'  , type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
 opt = parser.parse_args()
@@ -52,7 +52,6 @@ if opt.dataset in ['imagenet', 'folder', 'lfw']:
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
 elif opt.dataset == 'lsun':
-    opt.workers = 1 # need to do this because of an lmdb / python bug
     dataset = dset.LSUN(db_path=opt.dataroot, classes=['bedroom_train'],
                         transform=transforms.Compose([
                             transforms.Scale(opt.imageSize),
