6238eb6ae8d09cbe1b333ef4c32502a2f2564243
Alykhan Tejani <alykhan.tejani@gmail.com>
2017-01-04 22:47:00 +0000

    Add super resolution example
    Remove unused import
    
    initial version of SR model
    
    Rename load_training_set and load_test_set as get_training_set and get_test_set
    
    removed unused Lambda
    
    changed default download dir to dataset
    
    fixed call to Scale() - now takes in correct size
    
    use PIL.Image.split to get Y plane
    
    moved to super_resolution dir
    
    removing duplicate files
    
    added README and script to super_resolve an image
    
    update README.md
    
    update README.md
    
    update README.md
    
    update README.md
    
    upscale cb and cr channels too for display
    
    added orthogonal initialization and an extra layer
    
    update README
    
    update  README
    
    update  README
    
    out.float
    
    update README
    
    change .float -> .cpu

---
+++ super_resolution/dataset.py
@@ -0,0 +1,38 @@
+import torch.utils.data as data
+
+from os import listdir
+from os.path import join
+import numpy as np
+from PIL import Image
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
+
+
+def load_img(filepath):
+    img = Image.open(filepath).convert('YCbCr')
+    y, _, _ = img.split()
+    return y
+
+
+class DatasetFromFolder(data.Dataset):
+    def __init__(self, image_dir, input_transform=None, target_transform=None):
+        super(DatasetFromFolder, self).__init__()
+        self.image_filenames = [join(image_dir, x) for x in listdir(image_dir) if is_image_file(x)]
+
+        self.input_transform = input_transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        input = load_img(self.image_filenames[index])
+        target = input.copy()
+        if self.input_transform:
+            input = self.input_transform(input)
+        if self.target_transform:
+            target = self.target_transform(target)
+
+        return input, target
+
+    def __len__(self):
+        return len(self.image_filenames)
---
+++ super_resolution/main.py
@@ -0,0 +1,94 @@
+from __future__ import print_function
+import argparse
+from math import log10
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from model import Net
+from data import get_training_set, get_test_set
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
+parser.add_argument('--upscale_factor', type=int, required=True, help="super resolution upscale factor")
+parser.add_argument('--batchSize', type=int, default=64, help='training batch size')
+parser.add_argument('--testBatchSize', type=int, default=10, help='testing batch size')
+parser.add_argument('--nEpochs', type=int, default=2, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
+parser.add_argument('--cuda', action='store_true', help='use cuda?')
+parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
+parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
+opt = parser.parse_args()
+
+print(opt)
+
+cuda = opt.cuda
+if cuda and not torch.cuda.is_available():
+    raise Exception("No GPU found, please run without --cuda")
+
+torch.manual_seed(opt.seed)
+if cuda:
+    torch.cuda.manual_seed(opt.seed)
+
+print('===> Loading datasets')
+train_set = get_training_set(opt.upscale_factor)
+test_set = get_test_set(opt.upscale_factor)
+training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
+testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
+
+print('===> Building model')
+model = Net(upscale_factor=opt.upscale_factor)
+criterion = nn.MSELoss()
+
+if cuda:
+    model = model.cuda()
+    criterion = criterion.cuda()
+
+optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+
+
+def train(epoch):
+    epoch_loss = 0
+    for iteration, batch in enumerate(training_data_loader, 1):
+        input, target = Variable(batch[0]), Variable(batch[1])
+        if cuda:
+            input = input.cuda()
+            target = target.cuda()
+
+        optimizer.zero_grad()
+        loss = criterion(model(input), target)
+        epoch_loss += loss.data[0]
+        loss.backward()
+        optimizer.step()
+
+        print("===> Epoch[{}]({}/{}): Loss: {:.4f}".format(epoch, iteration, len(training_data_loader), loss.data[0]))
+
+    print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(epoch, epoch_loss / len(training_data_loader)))
+
+
+def test():
+    avg_psnr = 0
+    for batch in testing_data_loader:
+        input, target = Variable(batch[0]), Variable(batch[1])
+        if cuda:
+            input = input.cuda()
+            target = target.cuda()
+
+        prediction = model(input)
+        mse = criterion(prediction, target)
+        psnr = 10 * log10(1 / mse.data[0])
+        avg_psnr += psnr
+    print("===> Avg. PSNR: {:.4f} dB".format(avg_psnr / len(testing_data_loader)))
+
+
+def checkpoint(epoch):
+    model_out_path = "model_epoch_{}.pth".format(epoch)
+    torch.save(model, model_out_path)
+    print("Checkpoint saved to {}".format(model_out_path))
+
+for epoch in range(1, opt.nEpochs + 1):
+    train(epoch)
+    test()
+    checkpoint(epoch)
