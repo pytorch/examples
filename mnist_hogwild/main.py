c053f31a0577517c85136faf41802598d551a519
Adam Paszke <adam.paszke@gmail.com>
2017-01-17 21:21:03 +0100

    Fix dropout flag

--- mnist_hogwild/main.py
+++ mnist_hogwild/main.py
@@ -40,7 +40,7 @@ class Net(nn.Module):
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x)
+        x = F.dropout(x, training=self.training)
         x = F.relu(self.fc2(x))
         return F.log_softmax(x)
 
