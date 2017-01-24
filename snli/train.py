88508c0fb6791ac415f66405c78cd30b315d395a
Bryan Marcus McCann <bryan.mccann.is@gmail.com>
2017-01-24 21:35:52 +0000

    lowercasing and caching

--- snli/train.py
+++ snli/train.py
@@ -19,11 +19,19 @@ answers = data.Field(sequential=False)
 
 train, val, test = datasets.SNLI.splits(inputs, answers)
 
-inputs.build_vocab(train, vectors=(args.data_cache, args.word_vectors, args.d_embed))
+if os.path.isfile(args.vocab_cache):
+    inputs.build_vocab(train, lower=args.lower)
+    inputs.vocab.vectors = torch.load(args.vocab_cache)
+else:
+    inputs.build_vocab(train, vectors=(args.data_cache, args.word_vectors, args.d_embed), lower=args.lower)
+    os.makedirs(os.path.dirname(args.vocab_cache), exist_okay=True)
+    torch.save(inputs.vocab.vectors, args.vocab_cache)
 answers.build_vocab(train)
 
 train_iter, val_iter, test_iter = data.BucketIterator.splits(
             (train, val, test), batch_size=args.batch_size, device=args.gpu)
+print(train_iter.batch_size)
+print(len(train_iter))
 
 config = args
 config.n_embed = len(inputs.vocab)
@@ -33,8 +41,8 @@ if config.bidirectional:
     config.n_cells *= 2
 
 model = SNLIClassifier(config)
-if args.wv_path:
-    model.embed.weight = inputs.vocab.vectors
+if args.word_vectors:
+    model.embed.weight.data = inputs.vocab.vectors
 model.cuda()
 criterion = nn.CrossEntropyLoss()
 opt = O.Adam(model.parameters(), lr=args.lr)
@@ -42,12 +50,15 @@ opt = O.Adam(model.parameters(), lr=args.lr)
 iterations = 0
 start = time.time()
 best_dev_acc = -1
+train_iter.repeat = False
+header = '  Time Epoch Iteration Progress    (%Epoch)   Loss   Val/Loss     Accuracy  Val/Accuracy'
+val_log_template = ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>5.0f}/{:<5.0f} {:>7.0f}%,{:>8.6f},{:8.6f},{:12.4f},{:12.4f}'.split(','))
+log_template =     ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>5.0f}/{:<5.0f} {:>7.0f}%,{:>8.6f},{},{:12.4f},{}'.split(','))
+print(header)
+
 for epoch in range(args.epochs):
+    train_iter.init_epoch()
     n_correct, n_total = 0, 0
-    header = '  Time Epoch Iteration Progress    (%Epoch)   Loss   Val/Loss     Accuracy  Val/Accuracy'
-    val_log_template = ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>5.0f}/{:<5.0f} {:>7.0f}%,{:>8.6f},{:8.6f},{:12.4f},{:12.4f}'.split(','))
-    log_template =     ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>5.0f}/{:<5.0f} {:>7.0f}%,{:>8.6f},{},{:12.4f},{}'.split(','))
-    print(header)
     for batch_idx, batch in enumerate(train_iter):
         model.train(); opt.zero_grad()
         iterations += 1
@@ -58,7 +69,8 @@ for epoch in range(args.epochs):
         loss = criterion(answer, batch.label)
         loss.backward(); opt.step()
         if iterations % args.save_every == 0:
-            torch.save(model, os.path.join(args.save_path, 'snapshot_acc_{:.4f}_loss_{:.6f}_iter_{}_model.pt'.format(train_acc, loss.data[0], iterations)))
+            torch.save(model, os.path.join(args.save_path,
+                'snapshot_acc_{:.4f}_loss_{:.6f}_iter_{}_model.pt'.format(train_acc, loss.data[0], iterations)))
         if iterations % args.val_every == 0:
             model.eval(); val_iter.init_epoch()
             n_dev_correct, dev_loss = 0, 0
@@ -72,7 +84,8 @@ for epoch in range(args.epochs):
                 100. * batch_idx / len(train_iter), loss.data[0], dev_loss.data[0], train_acc, dev_acc))
             if dev_acc > best_dev_acc:
                 best_dev_acc = dev_acc
-                torch.save(model, os.path.join(args.save_path, 'best_devacc_{}_devloss_{}_model.pt'.format(dev_acc, dev_loss.data[0])))
+                torch.save(model, os.path.join(args.save_path,
+                    'best_devacc_{}_devloss_{}__iter_{}_model.pt'.format(dev_acc, dev_loss.data[0], iterations)))
         elif iterations % args.log_every == 0:
             print(log_template.format(time.time()-start,
                 epoch, iterations, batch_idx, len(train_iter),
