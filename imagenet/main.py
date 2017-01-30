ae23890322f5e57a8594ad32a5f97ce214ae3ae5
Junonia <pathbinder@yahoo.com>
2017-01-30 13:59:15 -0500

    Update main.py

--- imagenet/main.py
+++ imagenet/main.py
@@ -263,7 +263,7 @@ class AverageMeter(object):
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     lr = args.lr * (0.1 ** (epoch // 30))
-    for param_group in optimizer.state_dict()['param_groups']:
+    for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
 
