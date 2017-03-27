daafc5c5e6ead4cf54c537a88e8717cf0b2a2472
nadavbh12 <nadavbh@gmail.com>
2017-03-27 12:19:01 +0300

    Handle tied + dimensions mismatch

--- word_language_model/model.py
+++ word_language_model/model.py
@@ -26,6 +26,8 @@ class RNNModel(nn.Module):
         # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
         # https://arxiv.org/abs/1611.01462
         if tie_weights:
+            if nhid != ninp:
+                raise ValueError('When using the tied flag, nhid must be equal to emsize')
             self.decoder.weight = self.encoder.weight
 
         self.init_weights()
