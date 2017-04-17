42cdae7b2418c697614d8f8129c6074909147d6c
darkstar112358 <abhishekkadiyan@gmail.com>
2017-04-16 20:46:27 -0400

    Remove cloning of intermediate outputs

--- fast_neural_style/neural_style/vgg.py
+++ fast_neural_style/neural_style/vgg.py
@@ -22,11 +22,11 @@ class Vgg16(torch.nn.Module):
 
     def forward(self, X):
         h = self.slice1(X)
-        out1 = h.clone()
+        out1 = h
         h = self.slice2(h)
-        out2 = h.clone()
+        out2 = h
         h = self.slice3(h)
-        out3 = h.clone()
+        out3 = h
         h = self.slice4(h)
         out4 = h
         return [out1, out2, out3, out4]
