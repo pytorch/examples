7118a0755f6203b0d8eed8e7510c42f71e4d069e
darkstar112358 <abhishekkadiyan@gmail.com>
2017-05-04 18:43:53 -0400

    Change affine and momentum parameters for InstanceNorm

--- fast_neural_style/neural_style/transformer_net.py
+++ fast_neural_style/neural_style/transformer_net.py
@@ -6,11 +6,11 @@ class TransformerNet(torch.nn.Module):
         super(TransformerNet, self).__init__()
         # Initial convolution layers
         self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
-        self.in1 = torch.nn.InstanceNorm2d(32)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True, momentum=0)
         self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
-        self.in2 = torch.nn.InstanceNorm2d(64)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True, momentum=0)
         self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
-        self.in3 = torch.nn.InstanceNorm2d(128)
+        self.in3 = torch.nn.InstanceNorm2d(128, affine=True, momentum=0)
         # Residual layers
         self.res1 = ResidualBlock(128)
         self.res2 = ResidualBlock(128)
@@ -19,9 +19,9 @@ class TransformerNet(torch.nn.Module):
         self.res5 = ResidualBlock(128)
         # Upsampling Layers
         self.deconv1 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2)
-        self.in4 = torch.nn.InstanceNorm2d(64)
+        self.in4 = torch.nn.InstanceNorm2d(64, affine=True, momentum=0)
         self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2)
-        self.in5 = torch.nn.InstanceNorm2d(32)
+        self.in5 = torch.nn.InstanceNorm2d(32, affine=True, momentum=0)
         self.deconv3 = ConvLayer(32, 3, kernel_size=9, stride=1)
         # Non-linearities
         self.relu = torch.nn.ReLU()
@@ -63,9 +63,9 @@ class ResidualBlock(torch.nn.Module):
     def __init__(self, channels):
         super(ResidualBlock, self).__init__()
         self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
-        self.in1 = torch.nn.InstanceNorm2d(channels)
+        self.in1 = torch.nn.InstanceNorm2d(channels, affine=True, momentum=0)
         self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
-        self.in2 = torch.nn.InstanceNorm2d(channels)
+        self.in2 = torch.nn.InstanceNorm2d(channels, affine=True, momentum=0)
         self.relu = torch.nn.ReLU()
 
     def forward(self, x):
