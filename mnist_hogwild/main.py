55179e774d3c34c6cf358d216b632157918d15eb
Soumith Chintala <soumith@gmail.com>
2017-07-20 14:22:19 -0700

    0.2 fixes

--- mnist_hogwild/main.py
+++ mnist_hogwild/main.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.multiprocessing as mp
-
 from train import train
 
 # Training settings
--- mnist_hogwild/train.py
+++ mnist_hogwild/train.py
@@ -52,8 +52,8 @@ def test_epoch(model, data_loader):
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
         test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
-        pred = output.data.max(1)[1] # get the index of the max log-probability
-        correct += pred.eq(target.data).cpu().sum()
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     test_loss /= len(data_loader.dataset)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
