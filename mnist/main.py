0b61ca6f3904b1733d3f7c9b6dd5c356277f54bb
yueruchen <331607317@qq.com>
2017-08-25 21:52:19 +0800

    change some parameters

--- mnist/main.py
+++ mnist/main.py
@@ -99,7 +99,7 @@ def test():
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
         test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
-        pred = output.data.max(1,keepdim=True)[1] # get the index of the max log-probability
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     test_loss /= len(test_loader.dataset)
