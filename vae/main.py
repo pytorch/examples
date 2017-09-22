dc6e8ccb47bbc57d1fcb7c3a17af130d35b0aafb
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-09-22 15:27:26 +0900

    Fix wrong help messages

--- vae/main.py
+++ vae/main.py
@@ -9,9 +9,9 @@ from torchvision import datasets, transforms
 
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 64)')
+                    help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 2)')
+                    help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
