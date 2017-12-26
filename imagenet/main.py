c7ab2f86ab9aeac4367df97e4a4d686ef3b5ea4f
AIChallenger <zhenghe@interns.chuangxin.com>
2017-12-26 21:08:46 +0800

    batch_size 32

--- imagenet/main.py
+++ imagenet/main.py
@@ -38,7 +38,7 @@ parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=64, type=int,
+parser.add_argument('-b', '--batch-size', default=32, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate')
