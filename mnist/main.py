8256aee4449709cbe9713c70f911b122c146d95c
Lawrence Neal <lwneal@users.noreply.github.com>
2018-01-29 00:40:22 -0800

    Fix UserWarning in two examples (#293)
    * Fix UserWarning
    
    This fixes the following warning in mnist/main.py
    
    src/torch_mnist.py:68: UserWarning: Implicit dimension choice for log_softmax has been deprecated. Change the call to include dim=X as an argument.
    
    Performance is unaffected.
    
    * Fix UserWarning in mnist_hogwild
    
    In this case, dim=1 because the input tensor x has ndim=2.
    
    See _get_softmax_dim in https://github.com/pytorch/pytorch/blob/master/torch/nn/functional.py

--- mnist/main.py
+++ mnist/main.py
@@ -65,7 +65,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=1)
 
 model = Net()
 if args.cuda:
