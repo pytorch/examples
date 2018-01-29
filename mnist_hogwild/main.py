b359a8f8047242a7b8744bceb6fb2eb1e043e6ba
Larry Neal <nealla@lwneal.com>
2018-01-29 00:03:34 -0800

    Fix UserWarning in mnist_hogwild
    In this case, dim=1 because the input tensor x has ndim=2.
    
    See _get_softmax_dim in https://github.com/pytorch/pytorch/blob/master/torch/nn/functional.py

--- mnist_hogwild/main.py
+++ mnist_hogwild/main.py
@@ -42,7 +42,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x)
+        return F.log_softmax(x, dim=1)
 
 if __name__ == '__main__':
     args = parser.parse_args()
