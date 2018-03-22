dcdabc22b305d2f2989c6f03570dfcd3919e8a5b
Jason Park <diligensloth@gmail.com>
2018-03-23 00:11:00 +0900

    Fix arg of LSUN: db_path -> root (#319)

--- dcgan/main.py
+++ dcgan/main.py
@@ -64,7 +64,7 @@ if opt.dataset in ['imagenet', 'folder', 'lfw']:
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
 elif opt.dataset == 'lsun':
-    dataset = dset.LSUN(db_path=opt.dataroot, classes=['bedroom_train'],
+    dataset = dset.LSUN(root=opt.dataroot, classes=['bedroom_train'],
                         transform=transforms.Compose([
                             transforms.Resize(opt.imageSize),
                             transforms.CenterCrop(opt.imageSize),
