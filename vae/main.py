c7df3c00f10c60b9fadbfc6709d5799c40fcedc7
Xingdong Zuo <zuoxingdong@users.noreply.github.com>
2018-04-25 13:31:46 +0000

    VAE: use `F.sigmoid` as it is parameter-free (#338)
    * Update main.py
    
    * Update main.py
    
    * Update main.py

--- vae/main.py
+++ vae/main.py
@@ -47,24 +47,21 @@ class VAE(nn.Module):
         self.fc3 = nn.Linear(20, 400)
         self.fc4 = nn.Linear(400, 784)
 
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-
     def encode(self, x):
-        h1 = self.relu(self.fc1(x))
+        h1 = F.relu(self.fc1(x))
         return self.fc21(h1), self.fc22(h1)
 
     def reparameterize(self, mu, logvar):
         if self.training:
-            std = logvar.mul(0.5).exp_()
+            std = torch.exp(0.5*logvar)
             eps = torch.randn_like(std)
             return eps.mul(std).add_(mu)
         else:
             return mu
 
     def decode(self, z):
-        h3 = self.relu(self.fc3(z))
-        return self.sigmoid(self.fc4(h3))
+        h3 = F.relu(self.fc3(z))
+        return F.sigmoid(self.fc4(h3))
 
     def forward(self, x):
         mu, logvar = self.encode(x.view(-1, 784))
