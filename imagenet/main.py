56114a62cd9f35e23de2858226eb47fbc11f13de
Soumith Chintala <soumith@gmail.com>
2018-04-25 08:47:52 -0400

    Update main.py

--- imagenet/main.py
+++ imagenet/main.py
@@ -194,7 +194,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         # measure accuracy and record loss
         prec1, prec5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.item() input.size(0))
+        losses.update(loss.item(), input.size(0))
         top1.update(prec1[0], input.size(0))
         top5.update(prec5[0], input.size(0))
 
