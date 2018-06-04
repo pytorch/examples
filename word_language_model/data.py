f83508117b1ba9b752b227de992799093af3b215
siran <anmichel@gmail.com>
2018-06-04 11:02:40 -0400

    open file explicitely using utf-8 (#366)

--- word_language_model/data.py
+++ word_language_model/data.py
@@ -27,7 +27,7 @@ class Corpus(object):
         """Tokenizes a text file."""
         assert os.path.exists(path)
         # Add words to the dictionary
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding="utf8") as f:
             tokens = 0
             for line in f:
                 words = line.split() + ['<eos>']
@@ -36,7 +36,7 @@ class Corpus(object):
                     self.dictionary.add_word(word)
 
         # Tokenize file content
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding="utf8") as f:
             ids = torch.LongTensor(tokens)
             token = 0
             for line in f:
