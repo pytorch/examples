73631a770304749ba97019f6acaa6b6031597a12
an1006634493 <35823777+an1006634493@users.noreply.github.com>
2018-08-06 22:24:11 +0800

    Some little change on  full-connect layer
    use a new arg to make it more clear

--- mnist/main.py
+++ mnist/main.py
@@ -12,8 +12,8 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(320, hidden_layer)
+        self.fc2 = nn.Linear(hidden_layer, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
@@ -74,8 +74,11 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
+    parser.add_argument('--hidden_layer', type=int, default=50, metavar='N',
+                        help='how many hidden cells in the full-connected layer')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
+    hidden_layer = args.hidden_layer
 
     torch.manual_seed(args.seed)
 
