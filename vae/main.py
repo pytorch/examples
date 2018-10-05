95d5fddfb578674e01802f1db1820d8ac1015f67
Durk Kingma <dpkingma@gmail.com>
2018-10-04 21:58:04 -0700

    Fix bug in reparameterize() (#419)
    The reparameterize() function should not be deterministic for validation/test mode. It should be the same as in training mode, otherwise what is computed is not the ELBO (i.e. the correct loss) but something else. This change will slightly change the test set error.

--- vae/main.py
+++ vae/main.py
@@ -51,12 +51,9 @@ class VAE(nn.Module):
         return self.fc21(h1), self.fc22(h1)
 
     def reparameterize(self, mu, logvar):
-        if self.training:
-            std = torch.exp(0.5*logvar)
-            eps = torch.randn_like(std)
-            return eps.mul(std).add_(mu)
-        else:
-            return mu
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
 
     def decode(self, z):
         h3 = F.relu(self.fc3(z))
