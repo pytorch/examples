a6caf0610e969b0dee6c3ea6ba43a18923e1f955
Guanheng Zhang <zhangguanheng@devfair0435.h2.fair>
2019-04-19 12:51:30 -0700

    First transformer model working for word language model.

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -6,6 +6,7 @@ import os
 import torch
 import torch.nn as nn
 import torch.onnx
+import numpy as np
 
 import data
 import model
@@ -92,7 +93,25 @@ test_data = batchify(corpus.test, eval_batch_size)
 ###############################################################################
 
 ntokens = len(corpus.dictionary)
-model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.tied).to(device)
+#print("ntokens...", ntokens)
+#print("args.bptt...", args.bptt)
+
+#model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.tied).to(device)
+from torch.nn import Transformer 
+model = Transformer(ntokens, ntokens+1, nhead=4, d_model=args.emsize, num_encoder_layers=2, num_decoder_layers=2, d_ff=256).model   # plus one for "-1"
+#model.cuda()
+
+### Change the generator in transformer to nn.Linear(d_model, vocab)
+model.generator = nn.Linear(args.emsize, ntokens)
+
+### Modify embedding in encoder and decoder
+model.encoder.src_embed = nn.Embedding(ntokens, args.emsize)
+model.decoder.tgt_embed = nn.Embedding(ntokens, args.emsize)  # plus one for "-1"
+### Remove pos_encoder
+model.encoder.pos_encoder = None
+
+#print("ntokens is ", ntokens)
+#print("corpus.dictionary", corpus.dictionary.word2idx)
 
 criterion = nn.CrossEntropyLoss()
 
@@ -107,6 +126,12 @@ def repackage_hidden(h):
     else:
         return tuple(repackage_hidden(v) for v in h)
 
+def subsequent_mask(sz):
+    attn_shape = (1, sz, sz)
+    mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    mask = torch.from_numpy(mask) == 0
+    mask = mask[0].float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0)).view(sz, -1)
+    return mask
 
 # get_batch subdivides the source data into chunks of length args.bptt.
 # If source is equal to the example output of the batchify function, with
@@ -120,24 +145,53 @@ def repackage_hidden(h):
 
 def get_batch(source, i):
     seq_len = min(args.bptt, len(source) - 1 - i)
+    batch_size = source.size()[1]
     data = source[i:i+seq_len]
+#    decoder_inp = source[i:i+seq_len-1]
+#    decoder_inp = torch.cat((torch.ones(1, batch_size).long()*-1, decoder_inp), dim=0)
     target = source[i+1:i+1+seq_len].view(-1)
+#    print("data...", data.size(), data)
+#    print("target original...", source[i+1:i+1+seq_len])
+#    print("decoder_inp...", decoder_inp.size(), decoder_inp)
+#    print("target...", target.size(), target)
+#    target = source[-1].view(-1)
+#    return data, target, decoder_inp
     return data, target
 
+#def evaluate_model(model, src_data):
+#	tgt_input = src_data[0].unsqueeze(0)
+#	for _i in range(0, src_data.size()[0]):
+#		print("we are not evaluate the model on step ", _i)
+#		print("src_data...",src_data.size(),src_data)
+#		print("tgt_input...",tgt_input.size(),tgt_input)
+#		tgt_input = model(src_data, tgt_input)
+#	return tgt_input
 
 def evaluate(data_source):
     # Turn on evaluation mode which disables dropout.
     model.eval()
     total_loss = 0.
     ntokens = len(corpus.dictionary)
-    hidden = model.init_hidden(eval_batch_size)
+#    hidden = model.init_hidden(eval_batch_size)
+
+    tgt_mask = subsequent_mask(args.bptt)
+
     with torch.no_grad():
         for i in range(0, data_source.size(0) - 1, args.bptt):
             data, targets = get_batch(data_source, i)
-            output, hidden = model(data, hidden)
+
+            if args.bptt > len(data_source) - 1 - i:
+                tgt_mask = subsequent_mask(len(data_source) - 1 - i)
+
+#            print("data...", data.size(), data)
+#            print("tgt_mask...", tgt_mask.size(), tgt_mask)
+#            print("targets...", targets.size(), targets)
+#            output, hidden = model(data, hidden)
+            output = model(data, data, tgt_mask=tgt_mask)
             output_flat = output.view(-1, ntokens)
+#            output_flat = output[-1].view(-1, ntokens)
             total_loss += len(data) * criterion(output_flat, targets).item()
-            hidden = repackage_hidden(hidden)
+#            hidden = repackage_hidden(hidden)
     return total_loss / (len(data_source) - 1)
 
 
@@ -147,15 +201,42 @@ def train():
     total_loss = 0.
     start_time = time.time()
     ntokens = len(corpus.dictionary)
-    hidden = model.init_hidden(args.batch_size)
+#    hidden = model.init_hidden(args.batch_size)
+  
+    tgt_mask = subsequent_mask(args.bptt)
+
     for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
+#        data, targets, decoder_trg = get_batch(train_data, i)
         data, targets = get_batch(train_data, i)
+
+        if args.bptt > len(train_data) - 1 - i:
+            tgt_mask = subsequent_mask(len(train_data) - 1 - i)
+#        print("targets...", targets.size(), targets)
+#        print("data...", data.size(), data)
         # Starting each batch, we detach the hidden state from how it was previously produced.
         # If we didn't, the model would try backpropagating all the way to start of the dataset.
-        hidden = repackage_hidden(hidden)
+#        hidden = repackage_hidden(hidden)
         model.zero_grad()
-        output, hidden = model(data, hidden)
+#        output, hidden = model(data, hidden)
+
+#        print("data...",data.size())
+#        print("tgt_mask...",tgt_mask.size(),tgt_mask)
+#        print("decoder_trg...", decoder_trg.size())
+#        print("data and decoder_trg is same...", data[:-1]==decoder_trg[1:])
+        output = model(data, data, tgt_mask=tgt_mask)
+#        print("ntokens...", ntokens)
+#        print("output...", output.size(), output)
+#        print("targets...", targets.size(), targets)
+#        output = output[-1]
+
+#        print("output.view(-1, ntokens)...", output.view(-1, ntokens).size(), output.view(-1, ntokens))
+#        print("hidden...", hidden)
+#        print("targets...", targets.size(), targets)
+
+
         loss = criterion(output.view(-1, ntokens), targets)
+
+#        loss = criterion(output[-1].view(-1, ntokens), targets[-1])
         loss.backward()
 
         # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
@@ -212,12 +293,14 @@ except KeyboardInterrupt:
     print('-' * 89)
     print('Exiting from training early')
 
-# Load the best saved model.
-with open(args.save, 'rb') as f:
-    model = torch.load(f)
-    # after load the rnn params are not a continuous chunk of memory
-    # this makes them a continuous chunk, and will speed up forward pass
-    model.rnn.flatten_parameters()
+#
+## Load the best saved model.
+#with open(args.save, 'rb') as f:
+#    model = torch.load(f)
+#    # after load the rnn params are not a continuous chunk of memory
+#    # this makes them a continuous chunk, and will speed up forward pass
+#    model.rnn.flatten_parameters()
+#
 
 # Run on test data.
 test_loss = evaluate(test_data)
