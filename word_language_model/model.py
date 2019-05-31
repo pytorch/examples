8181f896b80ec8d586b9d8a96a3acdcace9092c2
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-05-31 11:10:37 -0700

    Fix a small bug in model.py.

--- word_language_model/model.py
+++ word_language_model/model.py
@@ -1,6 +1,7 @@
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
