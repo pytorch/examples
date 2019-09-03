5199a31c8b3f859d4c9ebdbe18799b8ab7fb4eed
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2019-09-04 01:11:24 +0530

    Update classify.cpp

--- cpp/transfer-learning/classify.cpp
+++ cpp/transfer-learning/classify.cpp
@@ -69,8 +69,8 @@ int main(int arc, char** argv)
     std::string model_path_linear = argv[3];
     
     // Load the model.
-    // type of model is torch::jit::script::Module
-    auto model = torch::jit::load(model_path);
+    // You can also use: auto model = torch::jit::load(model_path);
+    torch::jit::script::Module model = torch::jit::load(model_path);
     
     torch::nn::Linear model_linear(512, 2);
     torch::load(model_linear, model_path_linear);
