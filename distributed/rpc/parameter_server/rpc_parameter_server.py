a0da8d4e7dda4aacc66726e285dd4e340250dc47
Rohan Varma <rvarm1@fb.com>
2020-03-23 12:26:18 -0700

    update

--- distributed/rpc/parameter_server/rpc_parameter_server.py
+++ distributed/rpc/parameter_server/rpc_parameter_server.py
@@ -262,6 +262,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     assert args.rank is not None, "must provide rank argument."
+    assert args.num_gpus <= 3, f"Only 0-2 GPUs currently supported (got {args.num_gpus})."
     os.environ['MASTER_ADDR'] = args.master_addr
     os.environ["MASTER_PORT"] = args.master_port
     processes = []
