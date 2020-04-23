69d2798ec7fb4f87b320a1848203da5346675b95
Soumith Chintala <sc3104@nyu.edu>
2020-04-23 16:18:10 -0400

    Revert "loss detach (#731)" (#758)
    This reverts commit 391be73bbd191a08776c9ba05d29ef1e05c0c83b.

--- imagenet/main.py
+++ imagenet/main.py
@@ -289,7 +289,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
 
         # measure accuracy and record loss
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.detach().item(), images.size(0))
+        losses.update(loss.item(), images.size(0))
         top1.update(acc1[0], images.size(0))
         top5.update(acc5[0], images.size(0))
 
