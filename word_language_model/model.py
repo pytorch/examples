4e9172bcc9940cd6880a5599f5ce43e62d637605
Manash Kumar Mandal <manashmndl@gmail.com>
2020-05-22 01:42:31 +0600

    Fix initialization of weight of Decoder (#775)
    `nn.init.zeros_(self.decoder)` throws this error `AttributeError: module 'torch.nn.init' has no attribute 'zero_'` because the weights weren't passed.

--- word_language_model/model.py
+++ word_language_model/model.py
@@ -42,7 +42,7 @@ class RNNModel(nn.Module):
     def init_weights(self):
         initrange = 0.1
         nn.init.uniform_(self.encoder.weight, -initrange, initrange)
-        nn.init.zeros_(self.decoder)
+        nn.init.zeros_(self.decoder.weight)
         nn.init.uniform_(self.decoder.weight, -initrange, initrange)
 
     def forward(self, input, hidden):
