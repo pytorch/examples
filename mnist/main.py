1fbf2f72c8940eaf6334cbae819240e601da9c8b
bassbone <bassbone0o_o0@yahoo.co.jp>
2020-06-03 23:23:42 +0900

    Delete unnecessary blank lines

--- mnist/main.py
+++ mnist/main.py
@@ -87,7 +87,6 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
