592020bca81f7944d6deac11f83a8feb10289c8b
changbokLee <changbok2453@gmail.com>
2020-07-18 05:57:33 +0000

    Correct typo in default value within help

--- mnist/main.py
+++ mnist/main.py
@@ -89,7 +89,7 @@ def main():
                         help='quickly check a single pass')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+    parser.add_argument('--log-interval', type=int, default=14, metavar='N',
                         help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
