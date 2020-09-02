92b6945756c506ba55ecc6b04f0f6e08dd2a81b1
Natalia Gimelshein <ngimel@fb.com>
2020-09-01 17:38:09 -0700

    use reshape instead of view in accuracy

--- imagenet/main.py
+++ imagenet/main.py
@@ -420,7 +420,7 @@ def accuracy(output, target, topk=(1,)):
 
         res = []
         for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
