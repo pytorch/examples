97b5cc61b29f69b1b183b1434a02a201024e339a
Max van Dijck <50382570+MaxVanDijck@users.noreply.github.com>
2022-03-12 10:54:49 +1300

    add default imagenet directory

--- imagenet/main.py
+++ imagenet/main.py
@@ -24,8 +24,8 @@ model_names = sorted(name for name in models.__dict__
     and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-parser.add_argument('data', metavar='DIR',
-                    help='path to dataset')
+parser.add_argument('data', metavar='DIR', default='imagenet',
+                    help='path to dataset (default: imagenet)')
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                     choices=model_names,
                     help='model architecture: ' +
