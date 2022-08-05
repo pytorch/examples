2e150c0b69cc8767708ac1069e3e6174aad5ecd8
JoohyungLee0106 <43436960+JoohyungLee0106@users.noreply.github.com>
2022-08-05 15:47:39 +0900

    total must be CUDA
    total (ln 418) must be CUDA to compute dist.all_reduce() but now at cpu.

--- imagenet/main.py
+++ imagenet/main.py
@@ -415,7 +415,7 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
     def all_reduce(self):
-        total = torch.FloatTensor([self.sum, self.count])
+        total = torch.FloatTensor([self.sum, self.count]).cuda(self.sum.device)
         dist.all_reduce(total, dist.ReduceOp.SUM, async_op=False)
         self.sum, self.count = total.tolist()
         self.avg = self.sum / self.count
