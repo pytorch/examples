f45e418444b572ee5c782b90a0c2cae2489208bc
Suraj Subramanian <5676233+suraj813@users.noreply.github.com>
2022-09-22 09:56:25 -0400

    Add code for DDP tutorial series [PR 1 / 3] (#1067)

---
+++ distributed/ddp-tutorial-series/datautils.py
@@ -0,0 +1,13 @@
+import torch
+from torch.utils.data import Dataset
+
+class MyTrainDataset(Dataset):
+    def __init__(self, size):
+        self.size = size
+        self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
+
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, index):
+        return self.data[index]
