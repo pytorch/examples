3b8bb8eb4828745e02b4f27739acd9584475b27c
Suraj Subramanian <suraj813@gmail.com>
2022-09-22 12:24:21 -0400

    add refs to karpathy's repo

--- distributed/minGPT-ddp/mingpt/model.py
+++ distributed/minGPT-ddp/mingpt/model.py
@@ -1,11 +1,6 @@
 """
 Full definition of a GPT Language Model, all of it in this single file.
-
-References:
-1) the official GPT-2 TensorFlow implementation released by OpenAI:
-https://github.com/openai/gpt-2/blob/master/src/model.py
-2) huggingface/transformers PyTorch implementation:
-https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py
+Adapted from https://github.com/karpathy/minGPT/blob/master/mingpt/model.py
 """
 
 from dataclasses import dataclass
