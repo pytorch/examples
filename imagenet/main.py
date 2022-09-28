daaf5dcf43f2f3b750206cfd399c0f4fcf17a49d
zhi.cai <zhi.cai@mthreads.com>
2022-09-28 07:46:42 +0000

    Revert "þadapt Resnet50 training on mtGPU"
    This reverts commit 6b53cfa86559db88beb8724746511a3cad18ea25.

--- imagenet/main.py
+++ imagenet/main.py
@@ -7,7 +7,6 @@ import warnings
 from enum import Enum
 
 import torch
-import torchvision
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
@@ -21,12 +20,6 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 from torch.utils.data import Subset
-import musa_torch_extension
-import pdb
-from torch.utils.tensorboard import SummaryWriter
-
-# default `log_dir` is "runs" - we'll be more specific here
-writer = SummaryWriter('runs/mtgpu_cifar10_10_epoch')
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
@@ -46,7 +39,7 @@ parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=8, type=int,
+parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
                          'batch size of all GPUs on the current node when '
@@ -78,8 +71,6 @@ parser.add_argument('--seed', default=None, type=int,
                     help='seed for initializing training. ')
 parser.add_argument('--gpu', default=None, type=int,
                     help='GPU id to use.')
-parser.add_argument('-d', '--device', default=None, type=str,
-                    help='please specify the device as mtgpu if needed, default is None')
 parser.add_argument('--multiprocessing-distributed', action='store_true',
                     help='Use multi-processing distributed training to launch '
                          'N processes per node, which has N GPUs. This is the '
@@ -148,9 +139,7 @@ def main_worker(gpu, ngpus_per_node, args):
         print("=> creating model '{}'".format(args.arch))
         model = models.__dict__[args.arch]()
 
-    if args.device == "mtgpu":
-        model = model.to("mtgpu")
-    elif not torch.cuda.is_available():
+    if not torch.cuda.is_available():
         print('using CPU, this will be slow')
     elif args.distributed:
         # For multiprocessing distributed, DistributedDataParallel constructor
@@ -182,11 +171,7 @@ def main_worker(gpu, ngpus_per_node, args):
             model = torch.nn.DataParallel(model).cuda()
 
     # define loss function (criterion), optimizer, and learning rate scheduler
-    #criterion = nn.CrossEntropyLoss().cuda(args.gpu)
-    #criterion = nn.CrossEntropyLoss().cpu()
-    criterion = nn.CrossEntropyLoss()
-    if args.device == "mtgpu":
-        criterion = criterion.to("mtgpu")
+    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
@@ -221,25 +206,12 @@ def main_worker(gpu, ngpus_per_node, args):
     cudnn.benchmark = True
 
     # Data loading code
-    #traindir = os.path.join(args.data, 'train')
-    #valdir = os.path.join(args.data, 'val')
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
-    transform_cifar10 = transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize])
-    train_dataset = torchvision.datasets.CIFAR10(root = '../data',
-                                                 train=True,
-                                                 transform = transform_cifar10,
-                                                 download=True)
-    test_dataset = torchvision.datasets.CIFAR10(root = '../data',
-                                                 train=False,
-                                                 transform = transforms.ToTensor())
-    val_dataset = test_dataset
 
-    '''train_dataset = datasets.ImageFolder(
+    train_dataset = datasets.ImageFolder(
         traindir,
         transforms.Compose([
             transforms.RandomResizedCrop(224),
@@ -255,7 +227,7 @@ def main_worker(gpu, ngpus_per_node, args):
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             normalize,
-        ]))'''
+        ]))
 
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
@@ -303,9 +275,6 @@ def main_worker(gpu, ngpus_per_node, args):
                 'optimizer' : optimizer.state_dict(),
                 'scheduler' : scheduler.state_dict()
             }, is_best)
-    # save the whole model
-    torch.save(model, 'mtgpu_model_cifar10_10epoch.pth')
-    #model = torch.load('model.pth')
 
 
 def train(train_loader, model, criterion, optimizer, epoch, args):
@@ -323,7 +292,6 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
     model.train()
 
     end = time.time()
-    running_loss = 0.0
     for i, (images, target) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
@@ -332,9 +300,6 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
             images = images.cuda(args.gpu, non_blocking=True)
         if torch.cuda.is_available():
             target = target.cuda(args.gpu, non_blocking=True)
-        if args.device == "mtgpu":
-            images = images.to("mtgpu")
-            target = target.to("mtgpu")
 
         # compute output
         output = model(images)
@@ -350,16 +315,13 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        running_loss += loss.to("cpu").item()
+
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
 
         if i % args.print_freq == 0:
             progress.display(i + 1)
-        if i % 100 == 99:
-            writer.add_scalar('training loss', running_loss / 100, epoch * len(train_loader) + i)
-            running_loss = 0.0
 
 
 def validate(val_loader, model, criterion, args):
@@ -373,9 +335,6 @@ def validate(val_loader, model, criterion, args):
                     images = images.cuda(args.gpu, non_blocking=True)
                 if torch.cuda.is_available():
                     target = target.cuda(args.gpu, non_blocking=True)
-                if args.device == "mtgpu":
-                    images = images.to("mtgpu")
-                    target = target.to("mtgpu")
 
                 # compute output
                 output = model(images)
@@ -508,11 +467,9 @@ def accuracy(output, target, topk=(1,)):
         maxk = max(topk)
         batch_size = target.size(0)
 
-        # TopK is not ready on mtGPU now
-        output = output.to("cpu")
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred).to("cpu"))
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
 
         res = []
         for k in topk:
