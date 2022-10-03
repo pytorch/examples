2ee8d43dbe420be152fd5ce0d80b43b419a0e352
Suraj Subramanian <5676233+suraj813@users.noreply.github.com>
2022-10-03 15:55:25 -0400

    Add set_epoch and fix args in DDP-series example (#1076)
    Add set_epoch for shuffling inputs, fix arg order

--- distributed/minGPT-ddp/mingpt/trainer.py
+++ distributed/minGPT-ddp/mingpt/trainer.py
@@ -111,6 +111,7 @@ class Trainer:
         return loss.item()
 
     def _run_epoch(self, epoch: int, dataloader: DataLoader, train: bool = True):
+        self.dataloader.sampler.set_epoch(epoch)
         for iter, (source, targets) in enumerate(dataloader):
             step_type = "Train" if train else "Eval"
             source = source.to(self.local_rank)
