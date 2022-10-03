b183a2518cbe8421f8e9b58bfe020fcbc701ed40
Suraj Subramanian <suraj813@gmail.com>
2022-10-03 18:10:30 +0000

    Add set_epoch for shuffling inputs, fix arg order

--- distributed/ddp-tutorial-series/multigpu_torchrun.py
+++ distributed/ddp-tutorial-series/multigpu_torchrun.py
@@ -51,6 +51,7 @@ class Trainer:
     def _run_epoch(self, epoch):
         b_sz = len(next(iter(self.train_data))[0])
         print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        self.train_data.sampler.set_epoch(epoch)
         for source, targets in self.train_data:
             source = source.to(self.gpu_id)
             targets = targets.to(self.gpu_id)
--- distributed/ddp-tutorial-series/multinode.py
+++ distributed/ddp-tutorial-series/multinode.py
@@ -52,6 +52,7 @@ class Trainer:
     def _run_epoch(self, epoch):
         b_sz = len(next(iter(self.train_data))[0])
         print(f"[GPU{self.global_rank}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        self.train_data.sampler.set_epoch(epoch)
         for source, targets in self.train_data:
             source = source.to(self.local_rank)
             targets = targets.to(self.local_rank)
