bd042ad5081fd629dc10c2957ed3e407f0b352e7
Steven Liu <hudeven@gmail.com>
2022-10-27 20:37:07 +0000

    support dry_run in test()

--- mnist_rnn/main.py
+++ mnist_rnn/main.py
@@ -57,7 +57,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 break
 
 
-def test(model, device, test_loader):
+def test(args, model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
@@ -68,6 +68,8 @@ def test(model, device, test_loader):
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
+            if args.dry_run:
+                break
 
     test_loss /= len(test_loader.dataset)
 
@@ -127,7 +129,7 @@ def main():
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
+        test(args, model, device, test_loader)
         scheduler.step()
 
     if args.save_model:
