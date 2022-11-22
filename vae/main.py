725c982aa05967ea8fedf040720791738b765a6b
daming-lu <daming-lu@users.noreply.github.com>
2022-11-21 21:46:51 -0800

    Update main.py
    val data should not shuffle

--- vae/main.py
+++ vae/main.py
@@ -41,7 +41,7 @@ train_loader = torch.utils.data.DataLoader(
     batch_size=args.batch_size, shuffle=True, **kwargs)
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
+    batch_size=args.batch_size, shuffle=False, **kwargs)
 
 
 class VAE(nn.Module):
