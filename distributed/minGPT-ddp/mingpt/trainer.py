1b36393d6eb1e909efb321eaea7facf473c7e1c4
Suraj Subramanian <5676233+suraj813@users.noreply.github.com>
2022-11-29 12:10:10 -0500

    Update trainer.py (#1098)
    fixes #1093

--- distributed/minGPT-ddp/mingpt/trainer.py
+++ distributed/minGPT-ddp/mingpt/trainer.py
@@ -111,7 +111,7 @@ class Trainer:
         return loss.item()
 
     def _run_epoch(self, epoch: int, dataloader: DataLoader, train: bool = True):
-        self.dataloader.sampler.set_epoch(epoch)
+        dataloader.sampler.set_epoch(epoch)
         for iter, (source, targets) in enumerate(dataloader):
             step_type = "Train" if train else "Eval"
             source = source.to(self.local_rank)
