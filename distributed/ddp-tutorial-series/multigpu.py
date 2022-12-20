244e4eefb199f5e74e11d15c1b1c3186ccd2d313
Conrad Stack <4440627+ConradStack@users.noreply.github.com>
2022-12-20 02:21:54 -0600

    set type of batch_size argument to int in ddp-tutorial-series (#1104)
    set type of batch_size argument to int

--- distributed/ddp-tutorial-series/multigpu.py
+++ distributed/ddp-tutorial-series/multigpu.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='simple distributed training job')
     parser.add_argument('total_epochs', type=int, help='Total epochs to train the model')
     parser.add_argument('save_every', type=int, help='How often to save a snapshot')
-    parser.add_argument('--batch_size', default=32, help='Input batch size on each device (default: 32)')
+    parser.add_argument('--batch_size', default=32, type=int, help='Input batch size on each device (default: 32)')
     args = parser.parse_args()
     
     world_size = torch.cuda.device_count()
