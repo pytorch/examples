880909aaec5c27e9a6aba8f344d24c2d8743f241
向阳 <91933474+XiangYyang@users.noreply.github.com>
2023-04-04 18:22:19 +0800

    Add GPU option to command argument.
    Add GPU option to command argument. "--no-cuda" ( default is False ) and "--no-mps" ( default is False )

--- time_sequence_prediction/train.py
+++ time_sequence_prediction/train.py
@@ -15,12 +15,12 @@ class Sequence(nn.Module):
         self.lstm2 = nn.LSTMCell(51, 51)
         self.linear = nn.Linear(51, 1)
 
-    def forward(self, input, future = 0):
+    def forward(self, input, device, future = 0):
         outputs = []
-        h_t = torch.zeros(input.size(0), 51, dtype=torch.double)
-        c_t = torch.zeros(input.size(0), 51, dtype=torch.double)
-        h_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
-        c_t2 = torch.zeros(input.size(0), 51, dtype=torch.double)
+        h_t = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
+        c_t = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
+        h_t2 = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
+        c_t2 = torch.zeros(input.size(0), 51, dtype=torch.double).to(device)
 
         for input_t in input.split(1, dim=1):
             h_t, c_t = self.lstm1(input_t, (h_t, c_t))
@@ -39,20 +39,33 @@ class Sequence(nn.Module):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--steps', type=int, default=15, help='steps to run')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--no-mps', action='store_true', default=False,
+                        help='disables macOS GPU training')
     opt = parser.parse_args()
+    # 
+    # Use GPU to train?
+    use_cuda = not opt.no_cuda and torch.cuda.is_available()
+    use_mps = not opt.no_mps and torch.backends.mps.is_available()
+    if use_cuda:
+        device = torch.device("cuda")
+    elif use_mps:
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
     # set random seed to 0
     np.random.seed(0)
     torch.manual_seed(0)
     # load data and make training set
     data = torch.load('traindata.pt')
-    input = torch.from_numpy(data[3:, :-1])
-    target = torch.from_numpy(data[3:, 1:])
-    test_input = torch.from_numpy(data[:3, :-1])
-    test_target = torch.from_numpy(data[:3, 1:])
+    input = torch.from_numpy(data[3:, :-1]).to(device)
+    target = torch.from_numpy(data[3:, 1:]).to(device)
+    test_input = torch.from_numpy(data[:3, :-1]).to(device)
+    test_target = torch.from_numpy(data[:3, 1:]).to(device)
     # build the model
-    seq = Sequence()
-    seq.double()
-    criterion = nn.MSELoss()
+    seq = Sequence().to(device).double()
+    criterion = nn.MSELoss().to(device)
     # use LBFGS as optimizer since we can load the whole data to train
     optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
     #begin to train
@@ -60,7 +73,7 @@ if __name__ == '__main__':
         print('STEP: ', i)
         def closure():
             optimizer.zero_grad()
-            out = seq(input)
+            out = seq(input, device = device)
             loss = criterion(out, target)
             print('loss:', loss.item())
             loss.backward()
@@ -69,10 +82,10 @@ if __name__ == '__main__':
         # begin to predict, no need to track gradient here
         with torch.no_grad():
             future = 1000
-            pred = seq(test_input, future=future)
+            pred = seq(test_input, device = device, future=future)
             loss = criterion(pred[:, :-future], test_target)
             print('test loss:', loss.item())
-            y = pred.detach().numpy()
+            y = pred.cpu().detach().numpy()
         # draw the result
         plt.figure(figsize=(30,10))
         plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
@@ -81,8 +94,9 @@ if __name__ == '__main__':
         plt.xticks(fontsize=20)
         plt.yticks(fontsize=20)
         def draw(yi, color):
-            plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
-            plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
+            cpu_input = input.to('cpu')
+            plt.plot(np.arange(cpu_input.size(1)), yi[:cpu_input.size(1)], color, linewidth = 2.0)
+            plt.plot(np.arange(cpu_input.size(1), cpu_input.size(1) + future), yi[cpu_input.size(1):], color + ':', linewidth = 2.0)
         draw(y[0], 'r')
         draw(y[1], 'g')
         draw(y[2], 'b')
