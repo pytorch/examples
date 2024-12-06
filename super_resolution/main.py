de2b8348a36de6bbef8c458acf8228918fda44c0
IanLee <114343432+IanLee1024@users.noreply.github.com>
2024-12-06 13:59:13 +0800

    Fix runtime error caused by multiprocessing on Windows

--- super_resolution/main.py
+++ super_resolution/main.py
@@ -9,48 +9,6 @@ from torch.utils.data import DataLoader
 from model import Net
 from data import get_training_set, get_test_set
 
-# Training settings
-parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
-parser.add_argument('--upscale_factor', type=int, required=True, help="super resolution upscale factor")
-parser.add_argument('--batchSize', type=int, default=64, help='training batch size')
-parser.add_argument('--testBatchSize', type=int, default=10, help='testing batch size')
-parser.add_argument('--nEpochs', type=int, default=2, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
-parser.add_argument('--cuda', action='store_true', help='use cuda?')
-parser.add_argument('--mps', action='store_true', default=False, help='enables macOS GPU training')
-parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
-parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
-opt = parser.parse_args()
-
-print(opt)
-
-if opt.cuda and not torch.cuda.is_available():
-    raise Exception("No GPU found, please run without --cuda")
-if not opt.mps and torch.backends.mps.is_available():
-    raise Exception("Found mps device, please run with --mps to enable macOS GPU")
-
-torch.manual_seed(opt.seed)
-use_mps = opt.mps and torch.backends.mps.is_available()
-
-if opt.cuda:
-    device = torch.device("cuda")
-elif use_mps:
-    device = torch.device("mps")
-else:
-    device = torch.device("cpu")
-
-print('===> Loading datasets')
-train_set = get_training_set(opt.upscale_factor)
-test_set = get_test_set(opt.upscale_factor)
-training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
-testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
-
-print('===> Building model')
-model = Net(upscale_factor=opt.upscale_factor).to(device)
-criterion = nn.MSELoss()
-
-optimizer = optim.Adam(model.parameters(), lr=opt.lr)
-
 
 def train(epoch):
     epoch_loss = 0
@@ -86,7 +44,51 @@ def checkpoint(epoch):
     torch.save(model, model_out_path)
     print("Checkpoint saved to {}".format(model_out_path))
 
-for epoch in range(1, opt.nEpochs + 1):
-    train(epoch)
-    test()
-    checkpoint(epoch)
+
+if __name__ == '__main__':
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
+    parser.add_argument('--upscale_factor', type=int, required=True, help="super resolution upscale factor")
+    parser.add_argument('--batchSize', type=int, default=64, help='training batch size')
+    parser.add_argument('--testBatchSize', type=int, default=10, help='testing batch size')
+    parser.add_argument('--nEpochs', type=int, default=2, help='number of epochs to train for')
+    parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
+    parser.add_argument('--cuda', action='store_true', help='use cuda?')
+    parser.add_argument('--mps', action='store_true', default=False, help='enables macOS GPU training')
+    parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
+    parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
+    opt = parser.parse_args()
+    
+    print(opt)
+    
+    if opt.cuda and not torch.cuda.is_available():
+        raise Exception("No GPU found, please run without --cuda")
+    if not opt.mps and torch.backends.mps.is_available():
+        raise Exception("Found mps device, please run with --mps to enable macOS GPU")
+    
+    torch.manual_seed(opt.seed)
+    use_mps = opt.mps and torch.backends.mps.is_available()
+    
+    if opt.cuda:
+        device = torch.device("cuda")
+    elif use_mps:
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+    
+    print('===> Loading datasets')
+    train_set = get_training_set(opt.upscale_factor)
+    test_set = get_test_set(opt.upscale_factor)
+    training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
+    testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.testBatchSize, shuffle=False)
+    
+    print('===> Building model')
+    model = Net(upscale_factor=opt.upscale_factor).to(device)
+    criterion = nn.MSELoss()
+    
+    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+    
+    for epoch in range(1, opt.nEpochs + 1):
+        train(epoch)
+        test()
+        checkpoint(epoch)
