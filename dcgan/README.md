cf905499e085fa16dd94f53f36e6dceb9efefd83
dggaytan <diana.gaytan.munoz@intel.com>
2025-03-06 19:06:14 +0000

    Add XPU support for DCGAN example and README.md
    Signed-off-by: dggaytan <diana.gaytan.munoz@intel.com>

--- dcgan/README.md
+++ dcgan/README.md
@@ -20,31 +20,34 @@ python download.py -c bedroom
 ## Usage
 
 ```
-usage: main.py [-h] --dataset DATASET --dataroot DATAROOT [--workers WORKERS]
-               [--batchSize BATCHSIZE] [--imageSize IMAGESIZE] [--nz NZ]
-               [--ngf NGF] [--ndf NDF] [--niter NITER] [--lr LR]
-               [--beta1 BETA1] [--cuda] [--ngpu NGPU] [--netG NETG]
-               [--netD NETD] [--mps]
+usage: main.py [-h] --dataset DATASET [--dataroot DATAROOT] [--workers WORKERS] [--batchSize BATCHSIZE] [--imageSize IMAGESIZE]
+               [--nz NZ] [--ngf NGF] [--ndf NDF] [--niter NITER] [--lr LR] [--beta1 BETA1] [--cuda] [--xpu] [--dry-run]
+               [--ngpu NGPU] [--netG NETG] [--netD NETD] [--outf OUTF] [--manualSeed MANUALSEED] [--classes CLASSES] [--mps]
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
   --dataset DATASET     cifar10 | lsun | mnist |imagenet | folder | lfw | fake
   --dataroot DATAROOT   path to dataset
   --workers WORKERS     number of data loading workers
-  --batchSize BATCHSIZE input batch size
-  --imageSize IMAGESIZE the height / width of the input image to network
+  --batchSize BATCHSIZE
+                        input batch size
+  --imageSize IMAGESIZE
+                        the height / width of the input image to network
   --nz NZ               size of the latent z vector
-  --ngf NGF             number of filters in the generator
-  --ndf NDF             number of filters in the discriminator
+  --ngf NGF
+  --ndf NDF
   --niter NITER         number of epochs to train for
   --lr LR               learning rate, default=0.0002
   --beta1 BETA1         beta1 for adam. default=0.5
   --cuda                enables cuda
-  --mps                 enables macOS GPU
+  --xpu                 enables XPU training
+  --dry-run             check a single training cycle works
   --ngpu NGPU           number of GPUs to use
   --netG NETG           path to netG (to continue training)
   --netD NETD           path to netD (to continue training)
   --outf OUTF           folder to output images and model checkpoints
-  --manualSeed SEED     manual seed
+  --manualSeed MANUALSEED
+                        manual seed
   --classes CLASSES     comma separated list of classes for the lsun data set
+  --mps                 enables macOS GPU training
 ```
--- dcgan/main.py
+++ dcgan/main.py
@@ -26,6 +26,7 @@ parser.add_argument('--niter', type=int, default=25, help='number of epochs to t
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', default=False, help='enables cuda')
+parser.add_argument('--xpu', action='store_true', default=False, help='enables XPU training')
 parser.add_argument('--dry-run', action='store_true', help='check a single training cycle works')
 parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
@@ -56,6 +57,9 @@ if torch.cuda.is_available() and not opt.cuda:
 
 if torch.backends.mps.is_available() and not opt.mps:
     print("WARNING: You have mps device, to enable macOS GPU run with --mps")
+  
+if torch.xpu.is_available() and not opt.xpu:
+    print("WARNING: You have XPU device, to enable XPU training run with --xpu")
   
 if opt.dataroot is None and str(opt.dataset).lower() != 'fake':
     raise ValueError("`dataroot` parameter is required for dataset \"%s\"" % opt.dataset)
@@ -107,18 +111,28 @@ assert dataset
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
                                          shuffle=True, num_workers=int(opt.workers))
 use_mps = opt.mps and torch.backends.mps.is_available()
+use_xpu = opt.xpu and torch.xpu.is_available()
+
+##xpu support
 if opt.cuda:
     device = torch.device("cuda:0")
 elif use_mps:
     device = torch.device("mps")
+elif use_xpu:
+    device = torch.device("xpu")
+    print("Number of devices: ", torch.xpu.device_count())
 else:
     device = torch.device("cpu")
+
 
 ngpu = int(opt.ngpu)
 nz = int(opt.nz)
 ngf = int(opt.ngf)
 ndf = int(opt.ndf)
 
+
+print("DEVICE TO USE: ", device)
+print(ngpu)
 
 # custom weights initialization called on netG and netD
 def weights_init(m):
@@ -160,6 +174,8 @@ class Generator(nn.Module):
     def forward(self, input):
         if input.is_cuda and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        if input.is_xpu and self.ngpu > 1:
+            output = nn.DataParallel(self.main, input, range(self.ngpu))
         else:
             output = self.main(input)
         return output
@@ -200,6 +216,8 @@ class Discriminator(nn.Module):
     def forward(self, input):
         if input.is_cuda and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        if input.is_xpu and self.ngpu > 1:
+            output = nn.DataParallel(self.main, input, range(self.ngpu))
         else:
             output = self.main(input)
 
@@ -281,3 +299,5 @@ for epoch in range(opt.niter):
     # do checkpointing
     torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
     torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
+
+
